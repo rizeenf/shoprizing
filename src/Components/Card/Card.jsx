e5fdf1e676f758f44a0a7f5159435cb7db2f650d
rizeenf <rize.nf11@gmail.com>
2023-08-27 21:08:35 +0700

    add section

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Card.scss";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ item }) => {
+  return (
+    <div className="cards">
+      <img src={item.img1} alt="" />
+      <p>{item.title}</p>
+      <div className="details">
+        <div className="prices">
+          <span>${item.oldPrice} </span>
+          <span>${item.newPrice} </span>
+        </div>
+        <div className="addToCart">
+          <span>ADD</span>
+          <ShoppingCartOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -3,8 +3,47 @@ import "./Section.scss";
 import Card from "../Card/Card";
 
 const Section = ({ type }) => {
+  const products = [
+    {
+      id: 1,
+      title: "Peaky blinder's Coat",
+      img1: "https://images.pexels.com/photos/1206873/pexels-photo-1206873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2: "https://images.pexels.com/photos/1643025/pexels-photo-1643025.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      oldPrice: 52,
+      newPrice: 45,
+      newItem: true,
+    },
+    {
+      id: 2,
+      title: "Red Woman's Coat",
+      img1: "https://images.pexels.com/photos/158648/girl-coat-old-coat-brown-coat-158648.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1906877/pexels-photo-1906877.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      oldPrice: 42,
+      newPrice: 35,
+      newItem: false,
+    },
+    {
+      id: 3,
+      title: "Grey Woman's Coat",
+      img1: "https://images.pexels.com/photos/3538028/pexels-photo-3538028.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/2043592/pexels-photo-2043592.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      oldPrice: 33,
+      newPrice: 30,
+      newItem: false,
+    },
+    {
+      id: 4,
+      title: "Red Woman's Coat",
+      img1: "https://images.pexels.com/photos/3951783/pexels-photo-3951783.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/3951785/pexels-photo-3951785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      oldPrice: 58,
+      newPrice: 49,
+      newItem: true,
+    },
+  ];
+
   return (
-    <div className="section">
+    <div className="sections">
       <div className="top">
         <h2>{type} Products</h2>
         <p>
@@ -17,7 +56,9 @@ const Section = ({ type }) => {
         </p>
       </div>
       <div className="bottom">
-        <Card />
+        {products.map((item) => (
+          <Card item={item} key={item.id} />
+        ))}
       </div>
     </div>
   );
